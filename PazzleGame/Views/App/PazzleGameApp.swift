2afc283ec4b43416ba347d9c49b9a73ac2df5476
vyachgudzenko <vyach.gudzenko@gmail.com>
2023-02-26 02:17:42 +0200

    Initial commit

---
+++ PazzleGame/Views/App/PazzleGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PazzleGameApp.swift
+//  PazzleGame
+//
+//  Created by Вячеслав Гудзенко on 26.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct PazzleGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
